aa95429d360c5de1ee6ed220b4dbdde6c4254e38
Pascilla <109357685+Pascilla@users.noreply.github.com>
2022-07-15 19:13:04 +0530

    Add files via upload

---
+++ Primenumber.java
@@ -0,0 +1,16 @@
+package Week1.DAY1;
+
+public class Primenumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i = 13;
+float j = i % 2;
+if(j>0) {
+	System.out.println("It is the prime number");
+}else {
+	System.out.println("It is not the prime number");
+}
+	}
+
+}
